36f677a4d2bf64c38cec5ccafd749732c90af240
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2021-04-22 15:39:19 -0400

    Move where we install the service file, change the name of the generated udev input so our rule picks it up

--- ds4drv/uinput.py
+++ ds4drv/uinput.py
@@ -296,7 +296,7 @@ class UInputDevice(object):
                     events[ecodes.EV_REL].append(name)
                 self.mouse_rel[name] = 0.0
 
-        self.device = UInput(name=layout.name, events=events,
+        self.device = UInput(name="Wireless Controller", events=events,   #name=layout.name
                              bustype=layout.bustype, vendor=layout.vendor,
                              product=layout.product, version=layout.version)
         self.layout = layout
