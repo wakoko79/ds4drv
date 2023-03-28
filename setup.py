264e18d828e13c3c18c7fb2ba918f67b373dc2f2
Randel Capati <randelmc21@gmail.com>
2023-03-28 15:54:12 +0800

    Will install udevrules and default conf file

--- setup.py
+++ setup.py
@@ -3,11 +3,81 @@
 from setuptools import setup
 
 import sys
+import os
+import platform
+
+from distutils.util import execute
+from distutils.dir_util import copy_tree
+from distutils.file_util import copy_file
+from distutils.cmd import Command
+from subprocess import call
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools import setup, find_packages
 
 readme = open("README.rst").read()
 history = open("HISTORY.rst").read()
 
 sys.executable = "/usr/bin/python3"
+
+
+def udev_reload_rules():
+    call(["udevadm", "control", "--reload-rules"])
+
+
+def udev_trigger():
+    call(
+        [
+            "udevadm",
+            "trigger",
+        ]
+    )
+
+def install_udev_rules(raise_exception):
+    if check_root():
+        copy_tree("udev/", "/etc/udev/rules.d/")
+        copy_file("ds4drv.conf", "/etc/")
+        execute(udev_reload_rules, (), "Reloading udev rules")
+        execute(udev_trigger, (), "Triggering udev rules")
+    else:
+        msg = 'You must have root privileges to install udev rules. Run "sudo python setup.py udev_rules"'
+        if raise_exception:
+            raise OSError(msg)
+        else:
+            print(msg)
+
+
+def check_root():
+    return os.geteuid() == 0
+
+
+def is_linux():
+    return platform.system() == "Linux"
+
+
+class InstallUdevRules(Command):
+    description = "install udev rules (requires root privileges)"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        install_udev_rules(True)
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        install_udev_rules(True)
+
+
+class CustomDevelop(develop):
+    def run(self):
+        develop.run(self)
+
 
 setup(name="ds4drv",
       version="0.5.5",
@@ -36,5 +106,10 @@ setup(name="ds4drv",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Topic :: Games/Entertainment"
-      ]
+      ],
+      cmdclass={
+        "udev_rules": InstallUdevRules,
+        "install": CustomInstall,
+        "develop": CustomDevelop,
+      },
 )
